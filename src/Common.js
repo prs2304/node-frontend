ba1e9d2946eacede464457f25281b0dbcf2005f6
srinivassomesetty9 <125360992+srinivassomesetty9@users.noreply.github.com>
2023-03-29 15:23:22 +0530

    Update Common.js

--- src/Common.js
+++ src/Common.js
@@ -1,5 +1,5 @@
-// const baseUrl = 'http://65.0.169.55:5000'
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'http://65.0.101.134:5000'
+// const baseUrl = 'http://localhost:5000'
 
 export{
     baseUrl
